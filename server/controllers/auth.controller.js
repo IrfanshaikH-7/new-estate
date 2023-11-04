1e76e6bebb0e2543e01e75b7480f2a536abfb7b3
Irfan shaik <414irfann@gmail.com>
2023-11-04 19:43:19 +0530

    signIn page

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import  jwt  from 'jsonwebtoken';
 
 export const signup = async(req, res, next) => {
 
@@ -18,4 +20,26 @@ export const signup = async(req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const signin = async(req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser){
+            return next(errorHandler(404, "User not found"));
+        }
+        const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!isValidPassword){
+            return next(errorHandler(401, "Wrong credentials"))
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc; //destructureing everything except password for security reasons
+        res.cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest)
+    } catch (error) {
+        next(error)
+    }
+
 }
